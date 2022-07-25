0324652786eeefc4fca352f63be9305c2346d357
upskubra <yildirimk268@gmail.com>
2022-07-25 13:51:30 +0300

    set navigation

--- app/src/main/java/com/example/inviousgchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/inviousgchallenge/ui/MainActivity.kt
@@ -2,34 +2,49 @@ package com.example.inviousgchallenge.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.inviousgchallenge.adapter.ViewPagerAdapter
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.inviousgchallenge.R
 import com.example.inviousgchallenge.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private val fragmentsArray = arrayOf(
-        "Feed",
-        "Trash",
-        "Images"
-    )
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        setupViewPager()
-    }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.feed -> {
+                    navController.navigate(R.id.feedFragment)
+                    true
+                }
+                R.id.apiImage -> {
+                    navController.navigate(R.id.apiImagesFragment)
+                    true
+                }
+                R.id.trash -> {
+                    navController.navigate(R.id.trashFragment)
+                    true
 
-    fun setupViewPager() {
-        val viewPager = binding.pager
-        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
-        viewPager.adapter = adapter
-        TabLayoutMediator(binding.tabLayout, viewPager) { tab, position ->
-            tab.text = fragmentsArray[position]
-        }.attach()
+                }
+                else -> {
+                    navController.navigate(R.id.feedFragment)
+                    true
+                }
+
+            }
+
+        }
+
     }
 }
