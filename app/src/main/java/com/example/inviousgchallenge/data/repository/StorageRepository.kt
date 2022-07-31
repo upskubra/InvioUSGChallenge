11e318e890abfb1ab4df7f8d33a3d8c20b3dd096
upskubra <yildirimk268@gmail.com>
2022-08-01 00:42:31 +0300

    create StorageRepository

---
+++ app/src/main/java/com/example/inviousgchallenge/data/repository/StorageRepository.kt
@@ -0,0 +1,12 @@
+package com.example.inviousgchallenge.data.repository
+
+import android.net.Uri
+import com.example.inviousgchallenge.data.model.Image
+import com.example.inviousgchallenge.util.FirebaseState
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.Flow
+
+interface StorageRepository {
+    suspend fun getImages(): Flow<FirebaseState<ArrayList<Image>>>
+    suspend fun addImageToFirebaseStorage(imageUri: Uri): Flow<FirebaseState<Uri>>
+}
---
+++ app/src/main/java/com/example/inviousgchallenge/data/repository/StorageRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.example.inviousgchallenge.data.repository
+
+import android.net.Uri
+import com.example.inviousgchallenge.data.firebase.storage.StorageDataSource
+import com.example.inviousgchallenge.data.model.Image
+import com.example.inviousgchallenge.util.FirebaseState
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class StorageRepositoryImpl @Inject constructor(private val storageDataSource: StorageDataSource) :
+    StorageRepository {
+    override suspend fun getImages(): Flow<FirebaseState<ArrayList<Image>>> {
+        return storageDataSource.getImages()
+    }
+
+    override suspend fun addImageToFirebaseStorage(
+        imageUri: Uri,
+    ): Flow<FirebaseState<Uri>> {
+        return storageDataSource.addImageToFirebaseStorage(imageUri)
+    }
+}
