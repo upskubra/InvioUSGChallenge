e6fcf00ef02241a524a6e4e910d1f68c2ba04968
upskubra <yildirimk268@gmail.com>
2022-08-01 00:46:55 +0300

    add Constant

--- app/src/main/java/com/example/inviousgchallenge/util/Constants.kt
+++ app/src/main/java/com/example/inviousgchallenge/util/Constants.kt
@@ -2,4 +2,6 @@ package com.example.inviousgchallenge.util
 
 object Constants {
     const val API_KEY = "9141326c-6e7a-484b-8ba1-4499e3e30527"
+    // just for testing
+    const val UID = "awpyGuz0OSYA74RCcY4wfoRdJKq1"
 }
