56d4a586030dcb527d054e61c2185ceec0b1e2c6
upskubra <yildirimk268@gmail.com>
2022-08-09 17:43:05 +0300

    add BaseRemoteDataSource.kt

---
+++ app/src/main/java/com/example/inviousgchallenge/data/remote/BaseRemoteDataSource.kt
@@ -0,0 +1,25 @@
+package com.example.inviousgchallenge.data.remote
+
+import com.example.inviousgchallenge.util.NetworkResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import retrofit2.Response
+
+open class BaseRemoteDataSource {
+    protected suspend fun <T> getResult(call: suspend () -> Response<T>): Flow<NetworkResult<T>> {
+        return flow<NetworkResult<T>> {
+            val response = call()
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body != null) emit(NetworkResult.Success(body))
+            } else {
+                emit(NetworkResult.Error(response.errorBody().toString()))
+            }
+        }.catch {
+            emit(NetworkResult.Error(it.message ?: it.toString()))
+
+        }.onStart {
+            emit(NetworkResult.Loading())
+        }.flowOn(Dispatchers.IO)
+    }
+}
--- app/src/main/java/com/example/inviousgchallenge/data/remote/CatDataSourceImpl.kt
+++ app/src/main/java/com/example/inviousgchallenge/data/remote/CatDataSourceImpl.kt
@@ -1,32 +1,17 @@
 package com.example.inviousgchallenge.data.remote
 
 import com.example.inviousgchallenge.data.model.CatImageResponse
-import com.example.inviousgchallenge.data.remote.ApiService
-import com.example.inviousgchallenge.data.remote.CatDataSource
 import com.example.inviousgchallenge.util.NetworkResult
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 open class CatDataSourceImpl @Inject constructor(
     private val apiService: ApiService
 ) :
-    CatDataSource {
-    override suspend fun getCatImage(): Flow<NetworkResult<CatImageResponse>> = flow {
-        try {
-            emit(NetworkResult.Loading())
-            val response = apiService.getCatImage()
-            if (response.isSuccessful) {
-                emit(NetworkResult.Success(response.body()!!))
-            } else {
-                emit(NetworkResult.Error(response.message()))
-            }
-        } catch (e: HttpException) {
-            emit(NetworkResult.Error(e.message()))
-        } catch (e: IOException) {
-            emit(NetworkResult.Error("Couldn't reach server. Check your internet connection."))
+    CatDataSource, BaseRemoteDataSource() {
+    override suspend fun getCatImage(): Flow<NetworkResult<CatImageResponse>> {
+        return getResult {
+            apiService.getCatImage()
         }
     }
 }
